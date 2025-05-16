59d5a1c2e8c5840ac82cf3af06bcf51a1774a92e
inaaaas <147070778+inaaaas@users.noreply.github.com>
2025-05-16 15:17:43 +0400

    Update 2.c

--- 2.c
+++ 2.c
@@ -14,8 +14,13 @@ int main() {
     int num = 0;
     printf("Enter the numbers , which you want include to file \nEnter Q ,if you want to stop\n");
 
-    while (scanf("%d", &num) != 0) {
-        fprintf(file, "%d ", num);
+    // while (scanf("%d", &num) != 0) {
+    //     fprintf(file, "%d ", num);
+    // }
+    char buf[16];
+    while (scanf("%15s", buf)==1 && strcmp(buf,"Q")!=0) {
+      int d = atoi(buf);
+      fprintf(file,"%d ", d);
     }
     fclose(file);
     return 0;
